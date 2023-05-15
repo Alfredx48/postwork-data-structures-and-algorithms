fd82a2a0f68a103460034dd7132faebdd42918d4
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-05-15 14:20:54 -0700

    finished reverse string and find first dupe

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,6 +1,45 @@
-function reverseString(str) {
-  // type your code here
-}
+/**
+ * !PseudoCode
+  
+** Initialize an empty string 
+** loop over the input string backwards 
+** take each character and add it to the empty string 
+** return the string 
+*/
+
+// function reverseString(str) {
+//   return str.split("").reverse().join("")
+// }
+
+const reverseString = str => {
+	let reverseString = "";
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		reverseString += str[i];
+	}
+	return reverseString;
+};
+
+
+/** 
+ *! In place reverse 
+*/
+// function reverseString(str) {
+//   let charArray = str.split('')
+//   let left = 0
+//   let right = str.length - 1
+
+//   while (left < right) {
+//     console.log(left, right)
+//     let temp = charArray[left]
+//     charArray[left] = charArray[right]
+//     charArray[left] = temp
+
+//     left++
+//     right--
+//   }
+//   return charArray.join("")
+// }
 
 if (require.main === module) {
   // add your own tests in here
--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,16 +1,35 @@
+/**
+ *!PseudoCode
+
+ ** Create a Set (or hashtable) to store unique elements from the array.
+ ** Loop over the array.
+ ** If the current item from the array is already in the Set,
+ ** return the array item (first duplicate found).
+ ** Else, add it to the Set.
+ ** If the loop completes without finding a duplicate, return -1.
+*/
+
 function findFirstDuplicate(arr) {
-  // type your code here
+	const set = new Set();
+	for (let i = 0; i < arr.length; i++) {
+		if (set.has(arr[i])) {
+			return arr[i];
+		}
+		set.add(arr[i]);
+	}
+	return -1;
 }
 
+
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+	// add your own tests in here
+	console.log("Expecting: 3");
+	console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+	console.log("Expecting: -1");
+	console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
 }
 
 module.exports = findFirstDuplicate;
